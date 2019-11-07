000301ce367dff452817f8d8686d14cceadd24ea
AdamBialek <adambialek86@gmail.com>
2019-11-07 19:18:16 +0100

    add function getResponse in TodaysNews and connectionWithAPI in ApiConnectionService

--- src/main/java/com/businesscalendar/ApiConnectionService.java
+++ src/main/java/com/businesscalendar/ApiConnectionService.java
@@ -1,4 +1,25 @@
 package com.businesscalendar;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ApiConnectionService {
+    public String connectionWithAPI(String url) throws IOException {
+
+        HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(url).openConnection();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+
+        String line = "";
+        StringBuilder stringBuilder=new StringBuilder();
+        while ((line=bufferedReader.readLine())!=null){
+            stringBuilder.append(line);
+        }
+
+
+        return stringBuilder.toString();
+    }
 }
--- src/main/java/com/businesscalendar/TodaysNews.java
+++ src/main/java/com/businesscalendar/TodaysNews.java
@@ -1,5 +1,7 @@
 package com.businesscalendar;
 
+import java.io.IOException;
+
 public class TodaysNews {
         public final String Api_Url = "https://api.nytimes.com/svc/topstories/v2/";
 
@@ -8,6 +10,15 @@ public class TodaysNews {
         public final String Api_Key = ".json?api-key=xiE45x0Ko9i4PoeHRqEU9rGDYWi4AGjI";
 
         public String requestURL = new StringBuilder(Api_Url).append(Api_WhatNews).append(Api_Key).toString();
+
+        public String getResponse(String url) throws IOException {
+            String response = "";
+
+            response = new ApiConnectionService().connectionWithAPI(requestURL);
+
+            return response;
+        }
+
 
 
 }
