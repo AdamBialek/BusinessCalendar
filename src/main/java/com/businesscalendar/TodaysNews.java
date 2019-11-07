4896412ea70aa00154a61aaa2aeeba111f8bcb64
AdamBialek <adambialek86@gmail.com>
2019-11-07 18:34:01 +0100

    add ApiConnection Service and add URL to NYT API in TodaysNews

--- src/main/java/com/businesscalendar/TodaysNews.java
+++ src/main/java/com/businesscalendar/TodaysNews.java
@@ -1,4 +1,13 @@
 package com.businesscalendar;
 
 public class TodaysNews {
+        public final String Api_Url = "https://api.nytimes.com/svc/topstories/v2/";
+
+        public String Api_WhatNews = "business";
+
+        public final String Api_Key = ".json?api-key=xiE45x0Ko9i4PoeHRqEU9rGDYWi4AGjI";
+
+        public String requestURL = new StringBuilder(Api_Url).append(Api_WhatNews).append(Api_Key).toString();
+
+
 }
