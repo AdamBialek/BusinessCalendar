ea6f95f8dbee7f49f9e2946ea1735f581ca69b09
AdamBialek <adambialek86@gmail.com>
2019-11-08 22:06:31 +0100

    add start() to Main class and Anchor Pane to MainScreen. Main.main() now displays empty gui

--- src/main/java/com/businesscalendar/Main.java
+++ src/main/java/com/businesscalendar/Main.java
@@ -1,4 +1,23 @@
 package com.businesscalendar;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/fxml/MainScreen.fxml"));
+        AnchorPane anchorPane = fxmlLoader.load();
+        Scene scene=new Scene(anchorPane);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Business Calendar");
+        primaryStage.show();
+    }
 }
