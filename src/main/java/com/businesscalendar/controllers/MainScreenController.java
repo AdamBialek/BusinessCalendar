bf749b124c0d6c2eba0c0223c404f275bb23c344
AdamBialek <adambialek86@gmail.com>
2019-11-09 13:29:16 +0100

    all core functionality of NYT API already in place & some refactor in MainScreenController, some tweaks to controllers to MainScreen

--- src/main/java/com/businesscalendar/controllers/MainScreenController.java
+++ src/main/java/com/businesscalendar/controllers/MainScreenController.java
@@ -4,7 +4,6 @@ import com.businesscalendar.Article;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.image.ImageView;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,25 +11,13 @@ import java.util.List;
 
 public class MainScreenController {
 
-
     private Article article1;
 
     private Article article2;
 
     private Article article3;
 
-
     private int articlesToDisplay;
-
-    public Article getArticle1() {
-        return article1;
-    }
-
-    public String madeChoice;
-
-    public void setMadeChoice(String madeChoice) {
-        this.madeChoice = madeChoice;
-    }
 
     @FXML
     public List<Article> articleList;
@@ -47,16 +34,8 @@ public class MainScreenController {
         this.article1 = article1;
     }
 
-    public Article getArticle2() {
-        return article2;
-    }
-
     public void setArticle2(Article article2) {
         this.article2 = article2;
-    }
-
-    public Article getArticle3() {
-        return article3;
     }
 
     public void setArticle3(Article article3) {
@@ -70,8 +49,6 @@ public class MainScreenController {
     public void setSwitchArticles(int switchArticles) {
         this.articlesToDisplay = switchArticles;
     }
-
-
 
     @FXML
     private Button newer;
@@ -88,21 +65,85 @@ public class MainScreenController {
     @FXML
     private Hyperlink articleSpace3Link;
 
-    @FXML
-    private ImageView articleSpace1Img;
-
-    public void modifyArticlesToDisplay(int number){
-        setSwitchArticles(number);
-    }
-
     public void setArticles(){
-        if(articlesToDisplay==0){
         setArticle1(articleList.get(0));
         setArticle2(articleList.get(1));
         setArticle3(articleList.get(2));
-        }
     }
 
+    public void getHeadlines(){
+        List<Article> articleLinkedList = new LinkedList<>();
+        try {
+            articleLinkedList= article1.parseJson(article1.getResponse(article1.buildUrl()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setArticleList(articleLinkedList);
+        setArticles();
+        articleSpace1Link.setText(article1.getTitle());
+        articleSpace2Link.setText(article2.getTitle());
+        articleSpace3Link.setText(article3.getTitle());
+        setSwitchArticles(0);
+        newer.setDisable(true);
+        older.setDisable(false);
+    }
+
+    public void markLinksAsNotVisited(){
+        articleSpace1Link.setVisited(false);
+        articleSpace2Link.setVisited(false);
+        articleSpace3Link.setVisited(false);
+    }
+
+    @FXML
+    public void worldSwitch() {
+        article1.chooseNews("world");
+        article1.buildUrl();
+        markLinksAsNotVisited();
+        getHeadlines();
+    }
+
+
+    @FXML
+    public void businessSwitch() {
+        article1.chooseNews("business");
+        article1.buildUrl();
+        markLinksAsNotVisited();
+        getHeadlines();
+    }
+
+    @FXML
+    public void politicsSwitch() {
+        article1.chooseNews("politics");
+        article1.buildUrl();
+        markLinksAsNotVisited();
+        getHeadlines();
+    }
+
+    @FXML
+    public void techSwitch() {
+        article1.chooseNews("technology");
+        article1.buildUrl();
+        markLinksAsNotVisited();
+        getHeadlines();
+    }
+
+    @FXML
+    public void sportsSwitch() {
+        article1.chooseNews("sports");
+        article1.buildUrl();
+        markLinksAsNotVisited();
+        getHeadlines();
+    }
+
+    @FXML
+    public void travelSwitch() {
+        article1.chooseNews("travel");
+        article1.buildUrl();
+        markLinksAsNotVisited();
+        getHeadlines();
+    }
+
+    @FXML
     public void openUrl(String url) throws IOException {
         if(System.getProperty("os.name").toLowerCase().substring(0,3).equals("win")){
             Runtime rt = Runtime.getRuntime();
@@ -112,8 +153,6 @@ public class MainScreenController {
             rt.exec("open " + url);
         }
     }
-
-
 
     @FXML
     public void openLink1() throws IOException {
@@ -142,12 +181,12 @@ public class MainScreenController {
             articleSpace1Link.setText(article1.getTitle());
             articleSpace2Link.setText(article2.getTitle());
             articleSpace3Link.setText(article3.getTitle());
+            markLinksAsNotVisited();
             setSwitchArticles(now-3);
             older.setDisable(false);
         } else {
             newer.setDisable(true);
         }
-
     }
 
     @FXML
@@ -155,54 +194,33 @@ public class MainScreenController {
         int now = getSwitchArticles();
         int limit = getArticleList().size();
         System.out.println(now);
-        if(now<limit-5){
+        if(now<limit-6){
             setArticle1(articleList.get(now+3));
             setArticle2(articleList.get(now+4));
             setArticle3(articleList.get(now+5));
             articleSpace1Link.setText(article1.getTitle());
             articleSpace2Link.setText(article2.getTitle());
             articleSpace3Link.setText(article3.getTitle());
+            markLinksAsNotVisited();
             setSwitchArticles(now+3);
             newer.setDisable(false);
         } else {
             older.setDisable(true);
+            setArticle1(articleList.get(articleList.size()-3));
+            setArticle2(articleList.get(articleList.size()-2));
+            setArticle3(articleList.get(articleList.size()-1));
+            articleSpace1Link.setText(article1.getTitle());
+            articleSpace2Link.setText(article2.getTitle());
+            articleSpace3Link.setText(article3.getTitle());
+            markLinksAsNotVisited();
+            setSwitchArticles(now+3);
+            newer.setDisable(false);
         }
-
-
-
-
     }
-
 
     @FXML
     void initialize() throws IOException {
-        Article article=new Article();
-        List<Article> articleLinkedList = new LinkedList<>();
-        try {
-            articleLinkedList= article.parseJson(article.getResponse(article.buildUrl()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        setArticleList(articleLinkedList);
-        setArticles();
-        setArticle1(articleLinkedList.get(0));
-        articleSpace1Link.setText(article1.getTitle());
-        articleSpace2Link.setText(article2.getTitle());
-        articleSpace3Link.setText(article3.getTitle());
-
-
-        System.out.println(articleList.size());
-
-
-
-
-
-
-
-
-
-
-
-
+        article1 = new Article();
+        getHeadlines();
     }
 }
