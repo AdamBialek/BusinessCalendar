b3be12632949479fcab174a946a0840fd57dad67
AdamBialek <adambialek86@gmail.com>
2019-11-16 23:53:53 +0100

    linked buttons with functions and their types through FXML

--- src/main/java/com/businesscalendar/controllers/ChooseScreenController.java
+++ src/main/java/com/businesscalendar/controllers/ChooseScreenController.java
@@ -1,4 +1,21 @@
 package com.businesscalendar.controllers;
 
+import javafx.fxml.FXML;
+
 public class ChooseScreenController {
+
+    @FXML
+    public void previousNote() {}
+
+    @FXML
+    public void nextNote() {}
+
+    @FXML
+    public void enterNote() {}
+
+    @FXML
+    public void addNote() {}
+
+    @FXML
+    public void goBack() {}
 }
--- src/main/java/com/businesscalendar/controllers/NoteScreenController.java
+++ src/main/java/com/businesscalendar/controllers/NoteScreenController.java
@@ -1,4 +1,17 @@
 package com.businesscalendar.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class NoteScreenController {
+
+    @FXML
+    private TextField textContext;
+
+    @FXML
+    public void saveNote() {}
+
+    @FXML
+    public void goBack() {}
 }
