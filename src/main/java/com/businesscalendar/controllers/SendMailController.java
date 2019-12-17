98ae68d0708e89089634b547088d51556b39f9bf
AdamBialek <adambialek86@gmail.com>
2019-12-17 22:51:18 +0100

    add class SendMail that is mapped to button in menuBar

---
+++ src/main/java/com/businesscalendar/controllers/SendMailController.java
@@ -0,0 +1,16 @@
+package com.businesscalendar.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class SendMailController {
+
+    @FXML
+    private TextArea msgContent;
+
+    @FXML
+    public void sendMsg(){}
+
+    @FXML
+    public void back(){}
+}
